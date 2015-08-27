24f649ad36c99f61316f7cf957ad37e180d07551
Joel M. Smith <jxmxsx@gmail.com>
2015-08-27 14:14:48 -0700

    Moved recipe into using chef template for php.ini

---
+++ symplur/attributes/default.rb
@@ -0,0 +1,8 @@
+# PHP.ini Settings
+default['php']['ini_settings'] = {
+  'max_execution_time' => '1200',
+  'max_input_time' => '60',
+  'memory_limit' => '1G'
+}
+
+default['php']['apache_conf_dir'] = '/etc/php5/apache2'
--- symplur/recipes/phpini.rb
+++ symplur/recipes/phpini.rb
@@ -1,18 +1,21 @@
 Chef::Log.info('Hello from Symplur')
 
-# allows it to restart the apache2 service
 include_recipe "apache2::service"
- 
-# update php config
-file "/etc/php5/apache2/php.ini" do
-  content <<-EOH
+include_recipe "apache2::mod_php5"
 
-  ; Custom PHP Configuration
-  ; display_errors = "On"
-  memory_limit = #{node[:phpini][:memorylimit]}
-  max_execution_time = #{node[:phpini][:maxexecutiontime]}
-  max_input_time = #{node[:phpini][:maxinputtime]}
- 
-  EOH
-  notifies :restart, resources(:service => 'apache2')
+# reset config to production defaults
+#file "/etc/php5/apache2/php.ini" do
+#  owner 'root'
+#  group 'root'
+#  mode 0755
+#  content ::File.open("/usr/share/php5/php.ini-production").read
+# action :create
+#end 
+
+template "#{node['php']['apache_conf_dir']}/php.ini" do
+	source 'php.ini.erb'
+	owner 'root'
+	group 'root'
+	mode 00644
+	notifies :restart, resources(:service => 'apache2')
 end
