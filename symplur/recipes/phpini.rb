7d3f73d847fbc00edee7186fb26ddb68efc22069
Joel M. Smith <jxmxsx@gmail.com>
2015-08-27 13:21:04 -0700

    Attempt to have recipe add to php.ini

--- symplur/recipes/phpini.rb
+++ symplur/recipes/phpini.rb
@@ -1,1 +1,18 @@
 Chef::Log.info('Hello from Symplur')
+
+# allows it to restart the apache2 service
+include_recipe "apache2::service"
+ 
+# update php config
+file "/etc/php5/apache2/php.ini" do
+  content <<-EOH
+
+  ; Custom PHP Configuration
+  ; display_errors = "On"
+  memory_limit = #{node[:phpini][:memorylimit]}
+  max_execution_time = #{node[:phpini][:maxexecutiontime]}
+  max_input_time = #{node[:phpini][:maxinputtime]}
+ 
+  EOH
+  notifies :restart, resources(:service => 'apache2')
+end
